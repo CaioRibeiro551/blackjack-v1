1ef5f10a64b8a5550e4dcc489d8976feac065ca4
Caio <caio.h.r@outlook.com>
2024-02-15 14:59:20 -0300

    Acidionando biblioteca de imagem das cartas e implementando no código

--- src/game.clj
+++ src/game.clj
@@ -1,28 +1,27 @@
-(ns game)
+(ns game
+  (:require [card-ascii-art.core :as card]))
 
 ; Função que vai gerar carta pra mim
-; Cartas A,2,3,4,5,6,7,8,9,10,Q,J,K
+; Cartas A,2,3,4,5,6,7,8,9,10,J,Q,K,1....13
 
 (defn new-card []
   ;gerar numeros de cartas de 1 a 13
    (+(rand-int 13)1))
 
-(println (new-card))
 ; PROBLEMA É QUE EU NÃO QUERO O 0 E QUERO O 13 ,E ESTA INDO ATE O 12
 ; então vou somar 1 nos numeros que ele gerar
-
 
 ;como representar um jogador
 
 (defn player [player-name]
   (let [card1 (new-card)
         card2 (new-card)]
-    {:player player-name
+    {:player-name player-name
      :cards [card1 card2]}))
 
 
-(println (player "Caio"))
-(println (player "Dealer"))
+(card/print-player (player "Caio"))
+(card/print-player (player "Dealer"))
 
 
 
