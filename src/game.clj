b72e797cf9528c10fcb1f930e0c0b150425ba749
Caio <caio.h.r@outlook.com>
2024-02-15 21:13:19 -0300

    Implementação da regra de JKQ VALEM 10 e As vale 1 ou 11

--- src/game.clj
+++ src/game.clj
@@ -8,27 +8,70 @@
   ;gerar numeros de cartas de 1 a 13
    (+(rand-int 13)1))
 
+
 ; PROBLEMA É QUE EU NÃO QUERO O 0 E QUERO O 13 ,E ESTA INDO ATE O 12
 ; então vou somar 1 nos numeros que ele gerar
 
 ;como representar um jogador
 
 ; vai calcular os pontos de acordo com as cartas
-;1º REGRA  - J , Q , K = 10 (NÃO 11, 12 E 13)
-;2º REGRA  - [A 10] = 11 OU 21 = 21
-;3º REGRA - [A 5 7] = 1+5+7(13) OU 11+5+7 (23)
+;1º REGRA  - J , Q , K = 10
+;2º REGRA  - A = 11 ou 1
 
-
-
-;REGRA 1
-(defn JQK>10 [card]
-  (if (> card 10) 10 card))
 
 
 ;SOMA DOS PONTOS
 (defn points-cards [cards]
-  (let [cards-without-JQK (map JQK>10 cards)]
-    (reduce + cards-without-JQK)))
+  ;; Início da definição da função points-cards, que recebe uma lista de cartas como entrada.
+
+  (let [map-val {1 [1 11], 11 10, 12 10, 13 10}]
+    ;; Definimos um mapa chamado map-val que associa cada valor de carta ao seu valor numérico no blackjack.
+    ;; As chaves 11, 12 e 13 representam as cartas J, Q e K, respectivamente. Todos eles têm valor 10.
+    ;; A chave 1 representa o Ás, que pode ter valor 1 ou 11, dependendo da situação.
+
+    (letfn [(get-value [card]
+              ;; Definimos uma função interna chamada get-value que recebe um número de carta como entrada e retorna o valor correspondente no blackjack.
+
+              (if (<= card 10)
+                ;; Se a carta for um número de 2 a 10, seu valor é igual ao próprio número.
+                card
+                ;; Caso contrário, se for uma carta especial (J, Q, K, ou A), procuramos em map-val.
+
+                (get map-val card 10)))]
+      ;; Fim da definição da função interna get-value.
+
+      (let [sorted-cards (sort cards)
+            ;; Ordenamos as cartas recebidas em ordem crescente.
+
+            count-aces (count (filter #(= % 1) sorted-cards))
+            ;; Contamos quantos ases (cartas com valor 1) temos.
+
+            non-ace-cards (remove #(= % 1) sorted-cards)
+            ;; Retiramos os ases da lista de cartas.
+
+            non-ace-points (reduce + (map get-value non-ace-cards))]
+        ;; Calculamos a soma dos valores das cartas que não são ases.
+
+        (if (> count-aces 0)
+          ;; Se tivermos pelo menos um Ás na mão:
+
+          (let [min-points (+ non-ace-points count-aces)
+                ;; Calculamos os pontos mínimos assumindo que cada Ás vale 1.
+
+                max-points (+ non-ace-points (* count-aces 11))]
+            ;; Calculamos os pontos máximos assumindo que todos os ases valem 11.
+
+            (if (> max-points 21)
+              ;; Se os pontos máximos forem maiores que 21 (estouramos), usamos os pontos mínimos.
+              min-points
+              ;; Caso contrário, usamos os pontos máximos.
+              max-points))
+
+          ;; Se não tivermos ases na mão, usamos apenas os pontos das outras cartas.
+          non-ace-points)))))
+
+
+
 
 
 
